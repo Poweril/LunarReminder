bc35b5ac4cb2ddcbd0761cb7378396f5382548eb
Poweril <chaselynli@gmail.com>
2020-04-30 02:54:20 +0800

    Add settings activity

--- app/src/main/java/com/poweril/lunarreminder/MainActivity.java
+++ app/src/main/java/com/poweril/lunarreminder/MainActivity.java
@@ -1,5 +1,6 @@
 package com.poweril.lunarreminder;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
@@ -33,6 +34,8 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View view) {
                 Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
                         .setAction("Action", null).show();
+                Intent intent = new Intent(MainActivity.this, SettingsActivity.class);
+                startActivity(intent);
             }
         });
     }
